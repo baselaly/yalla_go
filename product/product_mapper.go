c42e933a6cf15c7b9a1bebd95189c5b21cd5b7e5
basel <basel.3ly@gmail.com>
2020-06-07 23:47:47 +0300

    transformed product

--- product/product_mapper.go
+++ product/product_mapper.go
@@ -1,1 +1,21 @@
 package product
+
+// TransformedProduct struct for transformed product
+type TransformedProduct struct {
+	ID          uint   `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	UserID      uint   `json:"user_id"`
+}
+
+// TransformProduct function to transform product
+func (p Product) TransformProduct() TransformedProduct {
+	var Tproduct TransformedProduct
+
+	Tproduct.ID = p.ID
+	Tproduct.Name = p.GetName()
+	Tproduct.Description = p.GetDescription()
+	Tproduct.UserID = p.GetUserID()
+
+	return Tproduct
+}
